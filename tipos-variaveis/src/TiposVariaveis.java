7d712035479ddbe775e523bc86c1c6a748cf5848
PAB1610 <pabloanast1@gmail.com>
2023-03-10 15:58:37 -0300

    Commit fim tipos variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.33;
+
+         //variavel float o valor atribuido tem que terminar com letra F
+         
+
+         //variavel do tipo long precisa terminar com a letra L
+         long cpf = 98765432109L;
+        // System.out.println(salarioMinimo +" - " + PI);
+
+         short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // fazer casting não é recomendado
+
+        // exemplo variavel constante 
+        final float PI = 3.14F;
+        
+        System.out.println(PI);
     }
 }
