a909570889e78f303109c591bd0b570a60600520
PAB1610 <pabloanast1@gmail.com>
2023-03-11 09:37:10 -0300

    Commit operadores java

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,74 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+
+        /*concatenação
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println(nomeCompleto);
+        String concatenacao ="?"; 
+        System.out.println(concatenacao);
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);*/
+
+
+        /* inversão sinal
+        int numero = 5;
+        numero = -numero; //metodo tornar numero positivo em negativo
+        System.out.println(numero);
+        numero = numero* (-1); //metodo tornar numero negativo em positivo
+        System.out.println(numero);*/
+     
+        /*//inversao valor booleano
+        boolean variavel = true;
+        System.out.println(variavel);
+        variavel=!variavel; 
+        System.out.println(variavel);
+        //somente com atribuição que o valor da variavel sera alterado*/
+
+       /*  //EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        String resultado = "";
+        int a, b;
+        a = 5;
+        b = 6;
+        
+        if(a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+
+            System.out.println(resultado);*/
+
+        /*// EXEMPLO DE CONDICIONAL UTILIZANDO //OPERADOR TERNARIO ?:
+        //só pode ser usado em operações booleanas
+        
+        int a = 6;
+        int b = 6;
+        //lê-se: se (a) igual a (b) string resultado recebe "verdadeiro" senão recebe "falso" 
+        String resultado = a==b ? "verdadeiro" : "falso";
+        System.out.println(resultado);*/
+
+       /*// EXEMPLO DE COMPARAÇÃO
+        int numero1=5;
+        int numero2=1;
+        //lê-se: numero 1 é igual a numero 2? a varival simNao recebe
+        // o resultado true ou false desta comparacao.
+        boolean simNao = numero1 == numero2;
+        System.out.println("Numero é igual ao numero 2? " + simNao);
+
+        if(numero1 > numero2)
+	        System.out.print("Numero 1 maior que numero 2");
+        else if (numero1 == numero2)
+             System.out.print("Numero 1 e numero 2 são iguais");
+        else
+            System.out.print("Numero 1 menor que numero 2"); */
+        
+    }
+}
